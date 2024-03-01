2ba20ff6b9c38d289924e8cc230db142f208bea5
Dedurin <grem85krbr@mail,ru>
2024-03-01 23:12:32 +0400

    RadioNew

--- src/main/java/ru/netology/service/Radio.java
+++ src/main/java/ru/netology/service/Radio.java
@@ -1,39 +1,38 @@
 package ru.netology.service;
 
 public class Radio {
-    private int currentWave;
+    private int maxStationNumber;
+    private int currentStation;
     private int currentVolume;
 
-    public void setCurrentWave(int newCurrentWave) {
-        if (newCurrentWave >= 0 && newCurrentWave <= 9) {
-            currentWave = newCurrentWave;
-        } else if (newCurrentWave < 0) {
-            currentWave = 0;
-        } else currentWave = 9;
+    public Radio() {
+        this.maxStationNumber = 10;
+        this.currentStation = 0;
+        this.currentVolume = 5;
     }
 
-    public int getCurrentWave() {
-        return currentWave;
+    public Radio(int maxStationNumber) {
+        this.maxStationNumber = maxStationNumber;
+        this.currentStation = 0;
+        this.currentVolume = 5;
     }
 
-    public void next() {
-        if (currentWave < 9) {
-            currentWave = currentWave + 1;
-        } else currentWave = 0;
+    public int getMaxStationNumber() {
+        return maxStationNumber;
     }
 
-    public void prev() {
-        if (currentWave > 0) {
-            currentWave = currentWave - 1;
-        } else currentWave = 9;
+    public void setMaxStationNumber(int maxStationNumber) {
+        this.maxStationNumber = maxStationNumber;
     }
 
-    public void setCurrentVolume(int newCurrentVolume) {
-        if (newCurrentVolume >= 0 && newCurrentVolume <= 10) {
-            currentVolume = newCurrentVolume;
-        } else if (newCurrentVolume < 0) {
-            currentVolume = 0;
-        } else currentVolume = 10;
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int station) {
+        if (station >= 0 && station < maxStationNumber) {
+            currentStation = station;
+        }
     }
 
     public int getCurrentVolume() {
@@ -41,15 +40,28 @@ public class Radio {
     }
 
     public void increaseVolume() {
-        if (currentVolume < 10) {
-            currentVolume = currentVolume + 1;
+        if (currentVolume < 100) {
+            currentVolume++;
         }
     }
 
     public void decreaseVolume() {
         if (currentVolume > 0) {
-            currentVolume = currentVolume - 1;
+            currentVolume--;
         }
     }
 
+    public void nextStation() {
+        currentStation = (currentStation + 1) % maxStationNumber;
+    }
+
+    public void prevStation() {
+        currentStation = (currentStation - 1 + maxStationNumber) % maxStationNumber;
+    }
+
+    public void setCurrentVolume(int volume) {
+        if (volume >= 0 && volume <= 100) {
+            currentVolume = volume;
+        }
+    }
 }
--- src/test/java/ru/netology/service/RadioTest.java
+++ src/test/java/ru/netology/service/RadioTest.java
@@ -3,105 +3,95 @@ package ru.netology.service;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 public class RadioTest {
 
     @Test
-    public void testSetCurrentWaveWithinRange() {
+    public void testDefaultRadioCreation() {
         Radio radio = new Radio();
-        radio.setCurrentWave(5);
-        assertEquals(5, radio.getCurrentWave());
-    }
-
-    @Test
-    public void testSetCurrentWaveBelowRange() {
-        Radio radio = new Radio();
-        radio.setCurrentWave(-1);
-        assertEquals(0, radio.getCurrentWave());
-    }
-
-    @Test
-    public void testSetCurrentWaveAboveRange() {
-        Radio radio = new Radio();
-        radio.setCurrentWave(10);
-        assertEquals(9, radio.getCurrentWave());
-    }
-
-    @Test
-    public void testNextWithinRange() {
-        Radio radio = new Radio();
-        radio.setCurrentWave(5);
-        radio.next();
-        assertEquals(6, radio.getCurrentWave());
-    }
-
-    @Test
-    public void testNextAtMaxValue() {
-        Radio radio = new Radio();
-        radio.setCurrentWave(9);
-        radio.next();
-        assertEquals(0, radio.getCurrentWave());
-    }
-
-    @Test
-    public void testPrevWithinRange() {
-        Radio radio = new Radio();
-        radio.setCurrentWave(5);
-        radio.prev();
-        assertEquals(4, radio.getCurrentWave());
-    }
-
-    @Test
-    public void testPrevAtMinValue() {
-        Radio radio = new Radio();
-        radio.setCurrentWave(0);
-        radio.prev();
-        assertEquals(9, radio.getCurrentWave());
-    }
-
-    @Test
-    public void testSetCurrentVolumeWithinRange() {
-        Radio radio = new Radio();
-        radio.setCurrentVolume(5);
+        assertEquals(10, radio.getMaxStationNumber());
+        assertEquals(0, radio.getCurrentStation());
         assertEquals(5, radio.getCurrentVolume());
     }
 
     @Test
-    public void testSetCurrentVolumeBelowRange() {
-        Radio radio = new Radio();
-        radio.setCurrentVolume(-1);
-        assertEquals(0, radio.getCurrentVolume());
+    public void testRadioCreationWithCustomMaxStationNumber() {
+        Radio radio = new Radio(15);
+        assertEquals(15, radio.getMaxStationNumber());
+        assertEquals(0, radio.getCurrentStation());
+        assertEquals(5, radio.getCurrentVolume());
     }
 
     @Test
-    public void testSetCurrentVolumeAboveRange() {
+    public void testSetMaxStationNumber() {
         Radio radio = new Radio();
-        radio.setCurrentVolume(11);
-        assertEquals(10, radio.getCurrentVolume());
+        radio.setMaxStationNumber(20);
+        assertEquals(20, radio.getMaxStationNumber());
+    }
+
+    @Test
+    public void testSetCurrentStationWithinRange() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(5);
+        assertEquals(5, radio.getCurrentStation());
+    }
+
+    @Test
+    public void testSetCurrentStationBelowRange() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(-1);
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    public void testSetCurrentStationAboveRange() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(12);
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    public void testNextStationWithinRange() {
+        Radio radio = new Radio();
+        radio.nextStation();
+        assertEquals(1, radio.getCurrentStation());
+    }
+
+    @Test
+    public void testPrevStationWithinRange() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(5);
+        radio.prevStation();
+        assertEquals(4, radio.getCurrentStation());
+    }
+
+    @Test
+    public void testPrevStationAtMinValue() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+        radio.prevStation();
+        assertEquals(9, radio.getCurrentStation());
     }
 
     @Test
     public void testIncreaseVolumeWithinRange() {
         Radio radio = new Radio();
-        radio.setCurrentVolume(5);
         radio.increaseVolume();
         assertEquals(6, radio.getCurrentVolume());
     }
 
     @Test
-    public void testIncreaseVolumeAtMaxValue() {
+    public void testDecreaseVolumeWithinRange() {
         Radio radio = new Radio();
-        radio.setCurrentVolume(10);
-        radio.increaseVolume();
-        assertEquals(10, radio.getCurrentVolume());
+        radio.decreaseVolume();
+        assertEquals(4, radio.getCurrentVolume());
     }
 
     @Test
-    public void testDecreaseVolumeWithinRange() {
+    public void testIncreaseVolumeAtMaxValue() {
         Radio radio = new Radio();
-        radio.setCurrentVolume(5);
-        radio.decreaseVolume();
-        assertEquals(4, radio.getCurrentVolume());
+        radio.setCurrentVolume(100);
+        radio.increaseVolume();
+        assertEquals(100, radio.getCurrentVolume());
     }
 
     @Test
@@ -110,5 +100,26 @@ public class RadioTest {
         radio.setCurrentVolume(0);
         radio.decreaseVolume();
         assertEquals(0, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void testSetCurrentVolumeWithinRange() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(50);
+        assertEquals(50, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void testSetCurrentVolumeAboveRange() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(150);
+        assertEquals(5, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void testSetCurrentVolumeBelowRange() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(-50);
+        assertEquals(5, radio.getCurrentVolume());
     }
 }
